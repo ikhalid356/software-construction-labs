afc2c10b86e751ca68a31b9b35b6c7eabf62acc2
DuaaZahra\Duaa <dkhan.bese15seecs@seecs.edu.pk>
2018-02-27 01:45:23 +0500

    Lab 3 Tasks

---
+++ Lab3/CMS/CMS/WebForm1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+namespace CMS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e) { }
+        public static string connectionString = "server=localhost;user id=root;password=root;persistsecurityinfo=True;database=lab2new";
+        public static string userID = ""; 
+    }
+}
